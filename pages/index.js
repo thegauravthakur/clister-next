4dfceabb5cbc0e74b4cc1de1b1e819fd8a7d133e
Gaurav Thakur <gthakur581@gmail.com>
2020-10-26 07:53:29 +0530

    corrected some of the typos

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,58 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Header from '../src/components/Header';
+import FeaturesSection1 from '../src/components/FeatureSection1';
+import FeaturesSection2 from '../src/components/FeatureSection2';
+import FeaturesSection3 from '../src/components/FeatureSection3';
+import EndSection from '../src/components/EndSection';
+import Footer from '../src/components/Footer';
+import Head from 'next/head';
 
-export default function Home() {
+const Home = ({isMobile}) => {
+
   return (
-    <div className={styles.container}>
+    <Paper style={{backgroundColor: 'black'}}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>CLister: HomePage</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <meta name="description"
+              content={'CLister is a cross-platform tool that will make you make your life more organized. It is fully customisable and all the data is synced safely on cloud'}/>
+        <meta charSet="utf-8"/>
+        {/*  social media links*/}
+        <meta property="og:title" content="C-LISTER: Clean Listing App" key={'ogtitle'}/>
+        <meta property="og:url" content="https://clister.in" key={'ogurl'}/>
+        <meta property="og:image"
+              content="https://res.cloudinary.com/gauravthakur/image/upload/v1603618699/CLISTER__CLEAN_LISTING_APP_2_1_rndenf.png"
+              key={'ogimage'}/>
+        <meta property="og:type" content="website" key={'ogtype'}/>
+        <meta property="og:description"
+              content="C-Lister is a cross-platform tool that will make your life more organized. It is fully customisable and all the data is synced safely on cloud"
+              key={'ogdescription'}/>
+        <meta property="og:locale" content="en_GB" key={'oglocale'}/>
+
+
       </Head>
+      <Header isMobile={isMobile}/>
+      <FeaturesSection1/>
+      <FeaturesSection2/>
+      <FeaturesSection3/>
+      <EndSection/>
+      <Footer/>
+    </Paper>
+  );
+};
+Home.getInitialProps = ({req}) => {
+  let userAgent;
+  if (req) { // if you are on the server and you get a 'req' property from your context
+    userAgent = req.headers['user-agent']; // get the user-agent from the headers
+  } else {
+    userAgent = navigator.userAgent; // if you are on the client you can access the navigator from the window object
+  }
+  let isMobile = Boolean(userAgent.match(
+    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+  ));
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  return {isMobile};
+};
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default Home;
